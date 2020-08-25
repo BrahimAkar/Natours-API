68e3ecfddf3a63dba3a9343450a215af441afd15
Brahim Akarouch <brahim.akarouch@gmail.com>
2020-08-25 14:06:19 +0100

    front-end login

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,46 @@
-/* eslint-disable */
-import axios from 'axios';
-import { showAlert } from './alerts';
+// /* eslint-disable */
+// import axios from 'axios';
+// import { showAlert } from './alerts';
 
-export const login = async (email, password) => {
-  try {
-    const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/login',
-      data: {
-        email,
-        password
-      }
-    });
+// export const login = async (email, password) => {
+//   try {
+//     const res = await axios({
+//       method: 'POST',
+//       url: '/api/v1/users/login',
+//       data: {
+//         email,
+//         password
+//       }
+//     });
 
-    if (res.data.status === 'success') {
-      showAlert('success', 'Logged in successfully!');
-      window.setTimeout(() => {
-        location.assign('/');
-      }, 1500);
-    }
-  } catch (err) {
-    showAlert('error', err.response.data.message);
-  }
-};
+//     if (res.data.status === 'success') {
+//       showAlert('success', 'Logged in successfully!');
+//       window.setTimeout(() => {
+//         location.assign('/');
+//       }, 1500);
+//     }
+//   } catch (err) {
+//     showAlert('error', err.response.data.message);
+//   }
+// };
 
-export const logout = async () => {
-  try {
-    const res = await axios({
-      method: 'GET',
-      url: '/api/v1/users/logout'
-    });
-    if ((res.data.status = 'success')) location.reload(true);
-  } catch (err) {
-    console.log(err.response);
-    showAlert('error', 'Error logging out! Try again.');
-  }
-};
+// export const logout = async () => {
+//   try {
+//     const res = await axios({
+//       method: 'GET',
+//       url: '/api/v1/users/logout'
+//     });
+//     if ((res.data.status = 'success')) location.reload(true);
+//   } catch (err) {
+//     console.log(err.response);
+//     showAlert('error', 'Error logging out! Try again.');
+//   }
+// };
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  console.log('Email', email);
+  console.log('Password', password);
+});
