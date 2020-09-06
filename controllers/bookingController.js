387d227eec9b12cddf50480166fa322197494d07
brahim <brahim.akarouch@gmail.com>
2020-09-06 02:40:15 +0000

    Fix Stripe timeOut error 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,13 +33,13 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-const createBookingCheckout = catchAsync(async session => {
+const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
   console.log('LOOK AT ME ', tour, user, price);
   await Booking.create({ tour, user, price });
-});
+};
 const webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
